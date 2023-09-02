aa6b86f58ade35d3475d78ef4a09ebe170630c78
TVHH579 <tranvohoanghuy12ab@gmail.com>
2023-09-02 18:07:18 +0700

    fix route get gift

--- src/routes/gift.js
+++ src/routes/gift.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const giftController = require('../controller/giftController');
 const checkRules = require('../middleware/checkRules');
-router.get('/list', checkRules.origin, giftController.getGiftList);
+router.get('/list', giftController.getGiftList);
 router.post('/add', checkRules.checkAdmin, giftController.addGift);
 router.delete(
     '/remove/:giftId',
