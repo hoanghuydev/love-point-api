1e82523594b17d9cc8ae9b5df446e8f42bf3cd84
TVHH579 <tranvohoanghuy12ab@gmail.com>
2023-09-02 13:13:00 +0700

    fix cor error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
